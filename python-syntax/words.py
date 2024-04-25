6241555a28f1875282c75955b2d584e0ec6d3c38
GGill97 <148311524+GGill97@users.noreply.github.com>
2024-04-24 22:31:16 -0700

    Add files via upload

---
+++ python-syntax/words.py
@@ -0,0 +1,12 @@
+def print_upper_words(words):
+    """Print each word on sep line, uppercased.
+
+        >>> print_upper_words(["Programming", "is", "pretty", "fun"])
+        PROGRAMMING
+        IS
+        PRETTY
+        FUN
+    """
+
+    for word in words:
+        print(word.upper())
